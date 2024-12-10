4159a1529f3afc6d1b74fa16f1862287d5b47abb
Debian <debian@vps-23e888df.vps.ovh.net>
2024-12-10 13:21:00 +0000

    Victory beach erbij

--- ngnix/new_cert.sh
+++ ngnix/new_cert.sh
@@ -3,7 +3,7 @@ docker run --rm \
   -v $(pwd)/html:/usr/share/nginx/html \
   certbot/certbot certonly --webroot \
   --webroot-path=/usr/share/nginx/html \
-  -d www.ygos.nl -d ygos.nl \
+  -d www.victorybeach.nl -d victorybeach.nl \
   --email stolkie@gmail.com \
   --agree-tos \
   --non-interactive
